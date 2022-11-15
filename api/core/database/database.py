fa7492ec68dcd62b7c785b40bfca3780a4ec11de
Mario Castro <mariocastro.pva@gmail.com>
2022-11-15 10:54:22 -0400

    fix database docker connection

--- api/core/database/database.py
+++ api/core/database/database.py
@@ -8,13 +8,8 @@ conf = Settings()
 
 
 async def init_db():
-    if conf.mongo_root_username != "" and conf.mongo_root_password != "":
-        client = motor.motor_asyncio.AsyncIOMotorClient(
-            f"mongodb://{conf.mongo_root_username}:{conf.mongo_root_password}@{conf.mongo_db_host}:{conf.mongo_db_port}/crm"
-        )
-    else:
-        client = motor.motor_asyncio.AsyncIOMotorClient(
-            f"mongodb://{conf.mongo_db_host}:{conf.mongo_db_port}/crm"
-        )
+    client = motor.motor_asyncio.AsyncIOMotorClient(
+        f"mongodb://{conf.mongo_root_username}:{conf.mongo_root_password}@{conf.mongo_db_host}:{conf.mongo_db_port}"
+    )
 
     await init_beanie(database=client.db_name, document_models=[Subscription])
